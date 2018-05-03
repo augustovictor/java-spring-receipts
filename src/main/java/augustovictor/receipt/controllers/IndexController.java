f7ef9a7244531e83f6e1c2b4542519dd65221ed5
Victor <victoraweb@gmail.com>
2018-05-03 08:57:08 -0300

    [TEST] Add simple category unit test

--- src/main/java/augustovictor/receipt/controllers/IndexController.java
+++ src/main/java/augustovictor/receipt/controllers/IndexController.java
@@ -5,12 +5,12 @@ import augustovictor.receipt.models.UnitOfMeasure;
 import augustovictor.receipt.repositories.CategoryRepository;
 import augustovictor.receipt.repositories.UnitOfMeasureRepository;
 import augustovictor.receipt.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -22,6 +22,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndex(Model model) {
+        log.debug("Received request");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
--- src/main/java/augustovictor/receipt/services/RecipeServiceImpl.java
+++ src/main/java/augustovictor/receipt/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package augustovictor.receipt.services;
 
 import augustovictor.receipt.models.Recipe;
 import augustovictor.receipt.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("Got in RecipeServiceImpl");
         Set<Recipe> recipes = new HashSet<>();
 
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
